0abb7307abd99f31a52e76e8226dfa6681945711
kulkarni42vivek <kulkarni42vivek@neml.in>
2023-06-17 11:48:27 +0530

    initial

---
+++ src/app/landing/landing.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LandingRoutingModule } from './landing-routing.module';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ChangePasswordComponent } from './change-password/change-password.component';
+import { LandingComponent } from './landing/landing.component';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { CheckboxModule } from 'primeng/checkbox';
+import { ChipModule } from 'primeng/chip';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { RippleModule } from 'primeng/ripple';
+import { TooltipModule } from 'primeng/tooltip';
+
+@NgModule({
+    declarations: [
+        LoginComponent,
+        RegisterComponent,
+        ChangePasswordComponent,
+        LandingComponent,
+    ],
+    imports: [
+        CommonModule,
+        LandingRoutingModule,
+        CommonModule,
+        ButtonModule,
+        RippleModule,
+        ChipModule,
+        CheckboxModule,
+        FormsModule,
+        InputTextModule,
+        PasswordModule,
+        TooltipModule,
+    ],
+})
+export class LandingModule {}
