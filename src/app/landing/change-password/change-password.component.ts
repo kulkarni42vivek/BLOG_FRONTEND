ddede4819cd827d394d2c8202f72a0f0753a6242
vivek <kulkarni42vivek@gmail.com>
2023-07-07 15:18:10 +0530

    cchang

--- src/app/landing/change-password/change-password.component.ts
+++ src/app/landing/change-password/change-password.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-change-password',
-  templateUrl: './change-password.component.html',
-  styleUrls: ['./change-password.component.scss']
+  templateUrl: './change-password.component.html'
 })
 export class ChangePasswordComponent {
 
